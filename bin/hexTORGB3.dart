0a1d0753a7200b2c417f6b34350c017cae309b72
Alberto José Domínguez Lizardo <albertojosedominguez348@gmail.com>
2020-12-19 15:19:24 -0500

    Add requirements document

---
+++ bin/hexTORGB3.dart
@@ -0,0 +1,5 @@
+import 'package:hexTORGB3/hexTORGB3.dart' as hexTORGB3;
+
+void main(List<String> arguments) {
+  print('Hello world: ${hexTORGB3.calculate()}!');
+}
---
+++ test/hexTORGB3_test.dart
@@ -0,0 +1,8 @@
+import 'package:hexTORGB3/hexTORGB3.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
