2127753c78e1964d3137eb24f3e88f9bf0ba8a49
Alberto José Domínguez Lizardo <albertojosedominguez348@gmail.com>
2020-12-19 15:25:59 -0500

    Add scenario 2 good cases

--- lib/hexTORGB3.dart
+++ lib/hexTORGB3.dart
@@ -1,5 +1,9 @@
 String getHexValue(var strRGB) {
   if (strRGB.length != 3) return 'Error';
+  var hexValue = '';
+  for (String x in strRGB) {
+    hexValue += int.parse(x).toRadixString(16).padLeft(2, '0').toUpperCase();
+  }
 
-  return strRGB.length;
+  return hexValue;
 }
--- test/hexTORGB3_test.dart
+++ test/hexTORGB3_test.dart
@@ -11,4 +11,14 @@ void main() {
     final expected = ['10', '255'];
     expect(getHexValue(expected), 'Error');
   });
+
+  test('good cases return hex color value', () {
+    final expected = ['10', '255', '2'];
+    expect(getHexValue(expected), '0AFF02');
+  });
+
+  test('good cases return hex color value 2', () {
+    final expected = ['10', '32', '2'];
+    expect(getHexValue(expected), '0A2002');
+  });
 }
